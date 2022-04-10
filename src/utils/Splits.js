5fe1023466a04a5972f8d0e455aa95927074ee5b
Sweets Sweetman <sweetmantech@gmail.com>
2022-04-10 16:53:32 -0300

    I init Splits util.

---
+++ src/utils/Splits.js
@@ -0,0 +1,68 @@
+import { ethers } from "ethers";
+import abi0xSplits from "./abi0xSplits.json";
+
+const SplitsService = {
+  getContract: (contractAddress, signer) =>
+    new ethers.Contract(contractAddress, abi0xSplits, signer),
+
+  createSplit: (
+    contract,
+    accounts,
+    percentAllocations,
+    distributorFee,
+    controller,
+    account
+  ) =>
+    contract.createSplit(
+      accounts,
+      percentAllocations,
+      distributorFee,
+      controller
+    ),
+
+  percentageScale: (contract, account) =>
+    contract.methods.PERCENTAGE_SCALE().call({ from: account }),
+
+  getSplitAccounts: (collaborators) => {
+    const accounts = collaborators.reduce(
+      (accountArray, currentCollaborator) => {
+        accountArray.push(currentCollaborator.col1);
+        return accountArray;
+      },
+      []
+    );
+    return accounts;
+  },
+
+  getSplitPercentAllocations: (collaborators) => {
+    const percentageAllocations = collaborators.reduce(
+      (percentsArray, currentCollaborator) => {
+        percentsArray.push(currentCollaborator.col2);
+        return percentsArray;
+      },
+      []
+    );
+    return percentageAllocations;
+  },
+
+  getScaledSplitPercentAllocations: (collaborators, scale) => {
+    const percentageAllocations = collaborators.reduce(
+      (percentsArray, currentCollaborator) => {
+        percentsArray.push(
+          (parseFloat(currentCollaborator.col2) * scale) / 100
+        );
+        return percentsArray;
+      },
+      []
+    );
+    return percentageAllocations;
+  },
+
+  sumSplits: (collaborators) =>
+    collaborators.reduce(
+      (partialSum, collaborator) =>
+        partialSum + (parseFloat(collaborator.col2) || 0),
+      0
+    ),
+};
+export default SplitsService;
